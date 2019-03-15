2ff3a28dc5dfc7966b81f05b8e0c59a571f779f9
vrancic <vladimir.rancic@chas.se>
2019-03-15 09:14:05 +0100

    added examples of JS function

--- src/js/routes/about.js
+++ src/js/routes/about.js
@@ -1,5 +1,15 @@
+//import dependecies
+
+// function
+// let someCustomFunction = () => {
+//   $('.custom').click(function(){
+//     alert('Test');
+//   })
+// };
+
 export default {
   init() {
+    // someCustomFunction();
     // JavaScript to be fired on the about us page
   },
 };
--- src/js/routes/common.js
+++ src/js/routes/common.js
@@ -1,6 +1,16 @@
+//import dependecies
+
+// function
+let someCustomFunction = () => {
+  $('.custom').click(function(){
+    alert('Test');
+  })
+};
+
 export default {
   init() {
     // JavaScript to be fired on all pages
+    someCustomFunction();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
